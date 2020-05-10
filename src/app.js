471ce59ce89eaf0bd528ce5bb130c6b8954f6328
Muhammad Sulton <musulton@gmail.com>
2020-05-10 22:28:27 +0700

    config(setup): convert es5 to es6, configuration dev dependencies babel and nodemon

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-var logger = require('morgan');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var indexRouter = require('./routes/index');
+import logger from 'morgan';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import indexRouter from './routes/index';
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -12,4 +12,4 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 
-module.exports = app;
+export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+const indexRouter = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+indexRouter.get('/', (req, res, next) => {
   return res.status(200).json({ message: 'Welcome to Hijra API' });
 });
 
-module.exports = router;
+export default indexRouter;
