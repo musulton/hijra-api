cc6cb573e9ebbcbec3bb325ddb2c22024655f2a4
Muhammad Sulton <musulton@gmail.com>
2020-05-10 22:34:15 +0700

    config(setup): configuration eslint and prettier

--- src/bin/www.js
+++ src/bin/www.js
@@ -8,12 +8,11 @@ import debug from 'debug';
 import http from 'http';
 import app from '../app';
 
-
 /**
  * Normalize a port into a number, string, or false.
  */
 
-const normalizePort = val => {
+const normalizePort = (val) => {
   const port = parseInt(val, 10);
   if (Number.isNaN(port)) {
     // named pipe
@@ -43,29 +42,27 @@ const server = http.createServer(app);
  * Event listener for HTTP server "error" event.
  */
 
-const onError = error => {
+const onError = (error) => {
   if (error.syscall !== 'listen') {
     throw error;
   }
 
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
+  case 'EACCES':
+    console.error(`${bind} requires elevated privileges`);
+    process.exit(1);
+    break;
+  case 'EADDRINUSE':
+    console.error(`${bind} is already in use`);
+    process.exit(1);
+    break;
+  default:
+    throw error;
   }
-}
+};
 
 /**
  * Event listener for HTTP server "listening" event.
@@ -73,11 +70,9 @@ const onError = error => {
 
 const onListening = () => {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+};
 
 /**
  * Listen on provided port, on all network interfaces.
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
+
 const indexRouter = express.Router();
 
 /* GET home page. */
-indexRouter.get('/', (req, res, next) => {
-  return res.status(200).json({ message: 'Welcome to Hijra API' });
-});
+indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Hijra API' }));
 
 export default indexRouter;
