7c097824504fb4a556d3e1cddfb943aeb6251eb7
Muhammad Sulton <musulton@gmail.com>
2020-05-11 20:51:51 +0700

    config(env): add env config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../settings';
 
 const indexRouter = express.Router();
 
 /* GET home page. */
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Hijra API' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
 
 export default indexRouter;
